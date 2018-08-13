3afaafb24a103c7696c8ce35e528864dfd5d2aff
Wilson Ng <wilson.ng@pascalinsights.com>
2018-08-13 17:48:46 -0400

    assignment 2 finished

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        invMat<-NULL
+        
+        setMat<-function(y){
+                x<<-y
+                invMat<<-NULL
+        }
+        getMat<-function(){
+                x
+        }
+        setInv<-function(inverse){
+                invMat<<-inverse
+        }
+        getInv<-function(){
+                invMat
+        }
+        list(setMat=setMat,getMat=getMat,setInv=setInv,getInv=getInv)
 
 }
 
@@ -12,4 +28,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        invMat<-x$getInv()
+        if(!is.null(invMat)) {
+                return(invMat)    
+        }
+        MatrixData<-x$getMat()    
+        invMatrix<-solve(MatrixData, ...)            
+        x$setInv(invMat)           
+
 }
